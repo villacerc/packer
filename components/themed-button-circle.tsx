6b66d8049bb54117efb4345a2c1a3ace686ee7cc
carlo pc <cvillaceran@gmail.com>
2026-02-19 14:10:02 -0800

    add trip details header

---
+++ components/themed-button-circle.tsx
@@ -0,0 +1,30 @@
+import { Colors } from "@/constants/theme";
+import { useAppScheme } from "@/hooks/use-app-scheme";
+import { StyleSheet, View, type ViewProps } from "react-native";
+
+export function ThemedButtonCircle({ style, ...otherProps }: ViewProps) {
+  const { scheme } = useAppScheme();
+  const themeStyles = scheme === "light" ? styles.light : styles.dark;
+  return (
+    <View style={[styles.container, style, themeStyles]} {...otherProps} />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 45,
+    height: 45,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+  },
+  light: {
+    backgroundColor: Colors.light.btnCircleBackground,
+    borderColor: Colors.light.btnCircleBorderColor,
+  },
+  dark: {
+    backgroundColor: Colors.dark.btnCircleBackground,
+    borderColor: Colors.dark.btnCircleBorderColor,
+  },
+});
---
+++ components/themed-icon.tsx
@@ -0,0 +1,32 @@
+import { useThemeColor } from "@/hooks/use-theme-color";
+import { Feather, MaterialIcons } from "@expo/vector-icons";
+import React from "react";
+import { type TextStyle } from "react-native";
+
+type IconLibrary = "feather" | "material";
+
+export type ThemedIconProps = {
+  name: string;
+  size?: number;
+  lightColor?: string;
+  darkColor?: string;
+  library?: IconLibrary;
+  style?: TextStyle;
+};
+
+export function ThemedIcon({
+  name,
+  size = 24,
+  lightColor,
+  darkColor,
+  library = "feather",
+  style,
+}: ThemedIconProps) {
+  const color = useThemeColor({ light: lightColor, dark: darkColor }, "text");
+
+  const IconComponent = library === "material" ? MaterialIcons : Feather;
+
+  return (
+    <IconComponent name={name as any} size={size} color={color} style={style} />
+  );
+}
